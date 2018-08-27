88b7bef62a3c642562b9f3c7359cd2f79b72c259
Michaelssss <liangyuming2@gmail.com>
2018-08-27 14:28:55 +0800

    启动配置文件更新

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 git pull;
-./mvnw clean compile package spring-boot::stop;
-./mvnw clean compile package spring-boot::start;
+./mvnw spring-boot::stop;
+./mvnw clean spring-boot::start;
