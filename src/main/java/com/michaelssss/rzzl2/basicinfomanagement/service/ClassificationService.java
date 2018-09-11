d17cce38fa37a8ff05949e4511dbe794c5231edc
Michaelssss <liangyuming2@gmail.com>
2018-09-11 14:34:14 +0800

    商票结构上传

--- src/main/java/com/michaelssss/rzzl2/basicinfomanagement/service/ClassificationService.java
+++ src/main/java/com/michaelssss/rzzl2/basicinfomanagement/service/ClassificationService.java
@@ -1,17 +1,28 @@
 package com.michaelssss.rzzl2.basicinfomanagement.service;
 
 import com.michaelssss.rzzl2.basicinfomanagement.ProductionClassification;
+import com.michaelssss.rzzl2.basicinfomanagement.domain.ProductionClassificationImpl;
 import com.michaelssss.rzzl2.basicinfomanagement.respository.ProductionClassificationCatalog;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ClassificationService {
     private ProductionClassificationCatalog classificationCatalog;
 
+    @Autowired
+    public ClassificationService(ProductionClassificationCatalog classificationCatalog) {
+        this.classificationCatalog = classificationCatalog;
+    }
+
     public List<ProductionClassification> getLevelOneProductionClassification() {
-        return null;
+        ProductionClassificationImpl sample = new ProductionClassificationImpl();
+        sample.setLevel(0);
+        return new ArrayList<>(classificationCatalog.findAll(Example.of(sample)));
     }
 
     public ProductionClassification getById(Long id) {
---
+++ src/main/java/com/michaelssss/rzzl2/creditmanagement/repository/TradeAcceptanceCatalog.java
@@ -0,0 +1,7 @@
+package com.michaelssss.rzzl2.creditmanagement.repository;
+
+import com.michaelssss.rzzl2.creditmanagement.domain.TradeAcceptance;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TradeAcceptanceCatalog extends JpaRepository<TradeAcceptance, Long> {
+}
