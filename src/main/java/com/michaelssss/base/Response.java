36057940f97c81895ac44209d77cabb87bce85e6
Michaelssss <liangyuming2@gmail.com>
2018-09-20 11:06:37 +0800

    User接口添加注解

--- src/main/java/com/michaelssss/base/Response.java
+++ src/main/java/com/michaelssss/base/Response.java
@@ -1,11 +1,7 @@
 package com.michaelssss.base;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import lombok.ToString;
-import org.springframework.data.domain.Page;
-
-import java.util.Collection;
 
 /**
  * Sample:
@@ -22,10 +18,6 @@ import java.util.Collection;
 public class Response<T> {
     private Status status;
     private T result;
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    private Integer currentPage;
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    private Integer total;
 
     private Response() {
     }
@@ -36,20 +28,7 @@ public class Response<T> {
         response.result = o;
         return response;
     }
-
-    /**
-     * 仅用于查询需要分页的业务数据使用
-     *
-     * @param collection 数据内容
-     * @param page       分页情况
-     * @return 结果
-     */
-    public static Response<?> OK(Collection<?> collection, Page page) {
-        Response response = OK(collection);
-        response.currentPage = page.getNumber();
-        response.total = page.getTotalPages();
-        return response;
-    }
+    
 
     public static Response<?> NonOK(Object o) {
         Response response = new Response<>();
